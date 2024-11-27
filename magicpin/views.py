ce101127e74c436f6af5f0b4184a1a1576b8cb41
JyotiKumari36 <jyotikumari11745@gmail.com>
2024-11-27 22:16:32 +0530

    Update views.py

--- magicpin/views.py
+++ magicpin/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def magicpin(request):
-    return HttpResponse("<h1>magicpin food </h1>")
+    return HttpResponse("<h1> Some time offer given by magicpin app</h1>")
 
