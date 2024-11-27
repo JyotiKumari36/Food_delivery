41ae3ea30412a0d3b2015d5fdedbc72b14502230
JyotiKumari36 <jyotikumari11745@gmail.com>
2024-11-27 22:11:46 +0530

    Update views.py

--- swiggy/views.py
+++ swiggy/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def yummy(request):
-    return HttpResponse("<h1 direction=right><marquee>Tendency to evoke feelings of hunger</marquee></h1>")
+    return HttpResponse("<h1><marquee direction=right>Tendency to evoke feelings of hunger</marquee></h1>")
 
