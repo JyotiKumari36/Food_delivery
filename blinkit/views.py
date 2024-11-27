60ac3b7eb1f9677b1e3606ff9576e4b69a300726
JyotiKumari36 <jyotikumari11745@gmail.com>
2024-11-27 22:23:28 +0530

    Update views.py

--- blinkit/views.py
+++ blinkit/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def blinkit(request):
-    return HttpResponse("<h1>blinkit food </h1>")
+    return HttpResponse("<h1>Blinkit delivered to you in 10 minutes.</h1>")
 
